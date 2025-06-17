2d4f31b3b3a3d6e332158946e77f54519711789e
Yogeshrmm2004 <yogeshmurugan1234@gmail.com>
2025-06-17 15:32:44 +0530

    exercise1 week1

---
+++ design principles/exercise1/SingletonPatternExample.java
@@ -0,0 +1,22 @@
+import java.util.*;
+// Singleton Pattern Example
+class Logger{
+    private static Logger instance;
+    private Logger(){
+        System.out.println("Logger initialized");
+    }
+    public static Logger getInstance(){
+        if(instance==null){
+            instance=new Logger();
+        }
+        return instance;
+    }
+}
+public class SingletonPatternExample {
+
+    public static void main(String[] args) {
+        Logger l1=Logger.getInstance();
+        Logger l2=Logger.getInstance();
+        System.out.print(l1==l2);
+    }
+}
